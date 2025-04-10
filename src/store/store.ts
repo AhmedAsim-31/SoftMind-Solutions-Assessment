ee02355c16985dd22b6371a0870e4bc01d07fb0c
Ahmed Asim <ahmedk082@gmail.com>
2025-04-10 19:31:20 +0500

    feat: redux setup for todo management

---
+++ src/store/store.ts
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import todoReducer from './todo-slice';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
+
+export const store = configureStore({
+  reducer: {
+    todos: persistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch; 
---
+++ src/store/todo-slice.ts
@@ -0,0 +1,69 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface Todo {
+  id: string;
+  name: string;
+  description: string;
+  dueDate: string;
+  dueTime: string;
+  completed: boolean;
+}
+
+interface TodoState {
+  todos: Todo[];
+  searchQuery: string;
+  sortBy: 'name' | 'date' | 'time';
+  sortOrder: 'asc' | 'desc';
+}
+
+const initialState: TodoState = {
+  todos: [],
+  searchQuery: '',
+  sortBy: 'date',
+  sortOrder: 'asc',
+};
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos.push(action.payload);
+    },
+    updateTodo: (state, action: PayloadAction<Todo>) => {
+      const index = state.todos.findIndex(todo => todo.id === action.payload.id);
+      if (index !== -1) {
+        state.todos[index] = action.payload;
+      }
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
+    },
+    toggleTodo: (state, action: PayloadAction<string>) => {
+      const todo = state.todos.find(todo => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    setSearchQuery: (state, action: PayloadAction<string>) => {
+      state.searchQuery = action.payload;
+    },
+    setSortBy: (state, action: PayloadAction<'name' | 'date' | 'time'>) => {
+      state.sortBy = action.payload;
+    },
+    setSortOrder: (state, action: PayloadAction<'asc' | 'desc'>) => {
+      state.sortOrder = action.payload;
+    },
+  },
+});
+
+export const {
+  addTodo,
+  updateTodo,
+  deleteTodo,
+  toggleTodo,
+  setSearchQuery,
+  setSortBy,
+  setSortOrder,
+} = todoSlice.actions;
+export default todoSlice.reducer; 
